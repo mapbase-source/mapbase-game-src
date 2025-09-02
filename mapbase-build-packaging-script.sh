7aa40b81fc08071cb5fe926d0ee40370446f32da
Blixibon <acj3017@gmail.com>
2025-09-02 18:14:12 -0500

    Update build packaging script for MP branch

--- mapbase-build-packaging-script.sh
+++ mapbase-build-packaging-script.sh
@@ -16,8 +16,8 @@
 #			folders in this repository. For episodic_materials_source, make sure the EP2 materials are converted
 #			after the EP1 materials so that the former group overwrites the latter.
 #
-#		3.	Find a previously released Mapbase build and extract the scenes from "mapbase_hl2/content/hl2_scenes"
-#			and "mapbase_episodic/content/episodic_scenes" to new "VPKs/hl2_scenes" and "VPKs/episodic_scenes"
+#		3.	Find a previously released Mapbase build and extract the scenes from "mapbase2_hl2/content/hl2_scenes"
+#			and "mapbase2_episodic/content/episodic_scenes" to new "VPKs/hl2_scenes" and "VPKs/episodic_scenes"
 #			folders in this repository. (This does not apply if you are planning on using a local code repo)
 #
 #		4.	Tweak the directory variables below.
@@ -33,7 +33,7 @@
 #			local copy of this repo, set USE_LOCAL_FGDS below to 'true' and set the path. That will eliminate
 #			this manual step.
 #
-#		-	You will need to include the legacy pre-v7.0 shared_content VPK in mapbase_shared. This can be
+#		-	You will need to include the legacy pre-v7.0 shared_content VPK in mapbase2_shared. This can be
 #			obtained from previously released builds.
 #
 # After generating a build, a to-do list showing any applicable manual steps will be given in the console.
@@ -43,10 +43,10 @@
 # =====================================================================================================
 
 # Where vpk.exe and its dependencies are on your computer
-BIN_DIR="C:/Program\ Files\ \(x86\)/Steam/SteamApps/common/Source\ SDK\ Base\ 2013\ Singleplayer/bin"
+BIN_DIR="C:/Program\ Files\ \(x86\)/Steam/SteamApps/common/Source\ SDK\ Base\ 2013\ Multiplayer/bin/x64"
 
 # This repository's 'VPKs' folder (used by vpk.exe)
-VPK_CONTENT_DIR="E:/Mapbase/mapbase-game-src/VPKs"
+VPK_CONTENT_DIR="E:/Mapbase/mapbase2-game-src/VPKs"
 
 # Whether or not to use a local source code repository to automatically gather DLLs
 USE_LOCAL_CODE=false
@@ -55,6 +55,7 @@ USE_LOCAL_CODE=false
 SOURCE_BIN_DIR="../mapbase-hl2-master/sp/game/bin"
 SOURCE_MOD_EPISODIC_DIR="../mapbase-hl2-master/sp/game/mod_episodic"
 SOURCE_MOD_HL2_DIR="../mapbase-hl2-master/sp/game/mod_hl2"
+SOURCE_MOD_TF_DIR="../mapbase-hl2-master/sp/game/mod_tf"
 
 # USE_LOCAL_CODE only: Where the original games' VCD files are (they come with the code repo by default)
 SOURCE_HL2_SCENES_DIR="../mapbase-hl2-master/sp/game/hl2/scenes"
@@ -69,8 +70,8 @@ SOURCE_FGDS_DIR="../source-fgds"
 
 # "read VERSION" could've been used for this, but it doesn't change as frequently as builds
 # are packaged, so it's only modified in the file and confirmed at runtime.
-VERSION="v8_0"
-VERSION_VPK_NAME="v7_0"
+VERSION="v1_0"
+VERSION_VPK_NAME="v1_0"
 
 # =====================================================================================================
 
@@ -92,12 +93,14 @@ echo "====================="
 echo "Copying loose folders"
 echo "====================="
 
-cp -r mapbase_episodic ${BUILD_DIR}
-echo "mapbase_episodic copied"
-cp -r mapbase_hl2 ${BUILD_DIR}
-echo "mapbase_hl2 copied"
-#cp mapbase_hl2mp ${BUILD_DIR}
-#echo "mapbase_hl2mp copied"
+cp -r mapbase2_episodic ${BUILD_DIR}
+echo "mapbase2_episodic copied"
+cp -r mapbase2_hl2 ${BUILD_DIR}
+echo "mapbase2_hl2 copied"
+cp -r mapbase2_hl2mp ${BUILD_DIR}
+echo "mapbase2_hl2mp copied"
+cp -r mapbase2_tf ${BUILD_DIR}
+echo "mapbase2_tf copied"
 
 if [ "$USE_LOCAL_CODE" = true ] ;
 then
@@ -106,27 +109,54 @@ then
 	echo "====================="
 	
 	echo "Validating bin folders..."
-	mkdir ${BUILD_DIR}/mapbase_episodic/bin
-	mkdir ${BUILD_DIR}/mapbase_hl2/bin
-	#mkdir ${BUILD_DIR}/mapbase_hl2mp/bin
+	mkdir ${BUILD_DIR}/mapbase2_episodic/bin
+	mkdir ${BUILD_DIR}/mapbase2_hl2/bin
+	mkdir ${BUILD_DIR}/mapbase2_hl2mp/bin
+	mkdir ${BUILD_DIR}/mapbase2_tf/bin
 	
 	echo "Copying binaries from mapbase-hl2-master..."
 	
-	cp ${SOURCE_MOD_EPISODIC_DIR}/bin/client.dll ${BUILD_DIR}/mapbase_episodic/bin
-	cp ${SOURCE_MOD_EPISODIC_DIR}/bin/server.dll ${BUILD_DIR}/mapbase_episodic/bin
-	cp ${SOURCE_MOD_EPISODIC_DIR}/bin/game_shader_dx9.dll ${BUILD_DIR}/mapbase_episodic/bin
+	cp ${SOURCE_MOD_EPISODIC_DIR}/bin/client.dll				${BUILD_DIR}/mapbase2_episodic/bin
+	cp ${SOURCE_MOD_EPISODIC_DIR}/bin/server.dll				${BUILD_DIR}/mapbase2_episodic/bin
+	cp ${SOURCE_MOD_EPISODIC_DIR}/bin/game_shader_dx9.dll		${BUILD_DIR}/mapbase2_episodic/bin
+	cp ${SOURCE_MOD_EPISODIC_DIR}/bin/x64/client.dll			${BUILD_DIR}/mapbase2_episodic/x64/bin
+	cp ${SOURCE_MOD_EPISODIC_DIR}/bin/x64/server.dll			${BUILD_DIR}/mapbase2_episodic/x64/bin
+	cp ${SOURCE_MOD_EPISODIC_DIR}/bin/x64/game_shader_dx9.dll	${BUILD_DIR}/mapbase2_episodic/x64/bin
 	
-	cp ${SOURCE_MOD_EPISODIC_DIR}/bin/client.so ${BUILD_DIR}/mapbase_episodic/bin
-	cp ${SOURCE_MOD_EPISODIC_DIR}/bin/server.so ${BUILD_DIR}/mapbase_episodic/bin
-	cp ${SOURCE_MOD_EPISODIC_DIR}/bin/game_shader_dx9.so ${BUILD_DIR}/mapbase_episodic/bin
+	cp ${SOURCE_MOD_EPISODIC_DIR}/bin/client.so						${BUILD_DIR}/mapbase2_episodic/bin
+	cp ${SOURCE_MOD_EPISODIC_DIR}/bin/server.so						${BUILD_DIR}/mapbase2_episodic/bin
+	cp ${SOURCE_MOD_EPISODIC_DIR}/bin/game_shader_dx9.so			${BUILD_DIR}/mapbase2_episodic/bin
+	cp ${SOURCE_MOD_EPISODIC_DIR}/bin/linux64/client.so				${BUILD_DIR}/mapbase2_episodic/linux64/bin
+	cp ${SOURCE_MOD_EPISODIC_DIR}/bin/linux64/server.so				${BUILD_DIR}/mapbase2_episodic/linux64/bin
+	cp ${SOURCE_MOD_EPISODIC_DIR}/bin/linux64/game_shader_dx9.so	${BUILD_DIR}/mapbase2_episodic/linux64/bin
 	
-	cp ${SOURCE_MOD_HL2_DIR}/bin/client.dll ${BUILD_DIR}/mapbase_hl2/bin
-	cp ${SOURCE_MOD_HL2_DIR}/bin/server.dll ${BUILD_DIR}/mapbase_hl2/bin
-	cp ${SOURCE_MOD_HL2_DIR}/bin/game_shader_dx9.dll ${BUILD_DIR}/mapbase_hl2/bin
+	cp ${SOURCE_MOD_HL2_DIR}/bin/client.dll						${BUILD_DIR}/mapbase2_hl2/bin
+	cp ${SOURCE_MOD_HL2_DIR}/bin/server.dll						${BUILD_DIR}/mapbase2_hl2/bin
+	cp ${SOURCE_MOD_HL2_DIR}/bin/game_shader_dx9.dll			${BUILD_DIR}/mapbase2_hl2/bin
+	cp ${SOURCE_MOD_HL2_DIR}/bin/x64/client.dll					${BUILD_DIR}/mapbase2_hl2/x64/bin
+	cp ${SOURCE_MOD_HL2_DIR}/bin/x64/server.dll					${BUILD_DIR}/mapbase2_hl2/x64/bin
+	cp ${SOURCE_MOD_HL2_DIR}/bin/x64/game_shader_dx9.dll		${BUILD_DIR}/mapbase2_hl2/x64/bin
 	
-	cp ${SOURCE_MOD_HL2_DIR}/bin/client.so ${BUILD_DIR}/mapbase_hl2/bin
-	cp ${SOURCE_MOD_HL2_DIR}/bin/server.so ${BUILD_DIR}/mapbase_hl2/bin
-	cp ${SOURCE_MOD_HL2_DIR}/bin/game_shader_dx9.so ${BUILD_DIR}/mapbase_hl2/bin
+	cp ${SOURCE_MOD_HL2_DIR}/bin/client.so						${BUILD_DIR}/mapbase2_hl2/bin
+	cp ${SOURCE_MOD_HL2_DIR}/bin/server.so						${BUILD_DIR}/mapbase2_hl2/bin
+	cp ${SOURCE_MOD_HL2_DIR}/bin/game_shader_dx9.so				${BUILD_DIR}/mapbase2_hl2/bin
+	cp ${SOURCE_MOD_HL2_DIR}/bin/linux64/client.so				${BUILD_DIR}/mapbase2_hl2/linux64/bin
+	cp ${SOURCE_MOD_HL2_DIR}/bin/linux64/server.so				${BUILD_DIR}/mapbase2_hl2/linux64/bin
+	cp ${SOURCE_MOD_HL2_DIR}/bin/linux64/game_shader_dx9.so		${BUILD_DIR}/mapbase2_hl2/linux64/bin
+	
+	cp ${SOURCE_MOD_TF_DIR}/bin/client.dll						${BUILD_DIR}/mapbase2_tf/bin
+	cp ${SOURCE_MOD_TF_DIR}/bin/server.dll						${BUILD_DIR}/mapbase2_tf/bin
+	cp ${SOURCE_MOD_TF_DIR}/bin/game_shader_dx9.dll				${BUILD_DIR}/mapbase2_tf/bin
+	cp ${SOURCE_MOD_TF_DIR}/bin/x64/client.dll					${BUILD_DIR}/mapbase2_tf/x64/bin
+	cp ${SOURCE_MOD_TF_DIR}/bin/x64/server.dll					${BUILD_DIR}/mapbase2_tf/x64/bin
+	cp ${SOURCE_MOD_TF_DIR}/bin/x64/game_shader_dx9.dll			${BUILD_DIR}/mapbase2_tf/x64/bin
+	
+	cp ${SOURCE_MOD_TF_DIR}/bin/client.so						${BUILD_DIR}/mapbase2_tf/bin
+	cp ${SOURCE_MOD_TF_DIR}/bin/server.so						${BUILD_DIR}/mapbase2_tf/bin
+	cp ${SOURCE_MOD_TF_DIR}/bin/game_shader_dx9.so				${BUILD_DIR}/mapbase2_tf/bin
+	cp ${SOURCE_MOD_TF_DIR}/bin/linux64/client.so				${BUILD_DIR}/mapbase2_tf/linux64/bin
+	cp ${SOURCE_MOD_TF_DIR}/bin/linux64/server.so				${BUILD_DIR}/mapbase2_tf/linux64/bin
+	cp ${SOURCE_MOD_TF_DIR}/bin/linux64/game_shader_dx9.so		${BUILD_DIR}/mapbase2_tf/linux64/bin
 	
 	echo "====================="
 	echo "Copying compile tools from SDK bin"
@@ -140,25 +170,6 @@ then
 	cp ${SOURCE_BIN_DIR}/vvis_dll.dll ${BUILD_DIR}/mapbase_engine_bin
 fi
 
-echo "====================="
-echo "Copying materials"
-echo "====================="
-
-cp -r VPKs/hl2_materials_source VPKs/hl2_materials
-echo "hl2_materials_source copied"
-cp -r VPKs/hl2_materials_overrides/materials VPKs/hl2_materials
-echo "hl2_materials_overrides copied"
-
-cp -r VPKs/episodic_materials_source VPKs/episodic_materials
-echo "episodic_materials_source copied"
-cp -r VPKs/episodic_materials_overrides/materials VPKs/episodic_materials
-echo "episodic_materials_overrides copied"
-
-cp -r VPKs/lostcoast_materials_source VPKs/lostcoast_materials
-echo "lostcoast_materials_source copied"
-cp -r VPKs/lostcoast_materials_overrides/materials VPKs/lostcoast_materials
-echo "lostcoast_materials_overrides copied"
-
 if [ "$USE_LOCAL_CODE" = true ] ;
 then
 	echo "====================="
@@ -168,11 +179,11 @@ then
 	mkdir VPKs/hl2_scenes
 	mkdir VPKs/episodic_scenes
 
-	cp -r ${SOURCE_HL2_SCENES_DIR} VPKs/hl2_scenes
+	cp -r ${SOURCE_HL2_SCENES_DIR} VPKs/mb2_hl2_scenes
 	echo "HL2 scenes copied"
-	cp -r ${SOURCE_EP1_SCENES_DIR} VPKs/episodic_scenes
+	cp -r ${SOURCE_EP1_SCENES_DIR} VPKs/mb2_episodic_scenes
 	echo "EP1 scenes copied"
-	cp -r ${SOURCE_EP2_SCENES_DIR} VPKs/episodic_scenes
+	cp -r ${SOURCE_EP2_SCENES_DIR} VPKs/mb2_episodic_scenes
 	echo "EP2 scenes copied"
 fi
 
@@ -180,54 +191,56 @@ echo "====================="
 echo "Packing VPKs"
 echo "====================="
 
-mkdir ${BUILD_DIR}/mapbase_shared
-mkdir ${BUILD_DIR}/mapbase_episodic/content
-mkdir ${BUILD_DIR}/mapbase_hl2/content
+mkdir ${BUILD_DIR}/mapbase2_shared
+mkdir ${BUILD_DIR}/mapbase2_episodic/content
+mkdir ${BUILD_DIR}/mapbase2_hl2/content
+mkdir ${BUILD_DIR}/mapbase2_hl2mp/content
+mkdir ${BUILD_DIR}/mapbase2_tf/content
 
 echo "Generating keypair for ${VERSION}"
-eval "${BIN_DIR}/vpk.exe generate_keypair \"mapbase_${VERSION}\""
+eval "${BIN_DIR}/vpk.exe generate_keypair \"mapbase2_${VERSION}\""
 
-eval "${BIN_DIR}/vpk.exe -M -P -c 50 -k mapbase_${VERSION}.publickey.vdf -K mapbase_${VERSION}.privatekey.vdf -vpk \"${VPK_CONTENT_DIR}/shared_content_${VERSION_VPK_NAME}\""
-eval "${BIN_DIR}/vpk.exe rehash \"${VPK_CONTENT_DIR}/shared_content_${VERSION_VPK_NAME}\""
-echo "Moving shared_content_${VERSION_VPK_NAME}"
-mv VPKs/*.vpk ${BUILD_DIR}/mapbase_shared
+eval "${BIN_DIR}/vpk.exe -M -P -c 50 -k mapbase2_${VERSION}.publickey.vdf -K mapbase2_${VERSION}.privatekey.vdf -vpk \"${VPK_CONTENT_DIR}/mb2_shared_content\""
+eval "${BIN_DIR}/vpk.exe rehash \"${VPK_CONTENT_DIR}/mb2_shared_content\""
+echo "Moving mb2_shared_content"
+mv VPKs/*.vpk ${BUILD_DIR}/mapbase2_shared
 
-eval "${BIN_DIR}/vpk.exe -M -P -c 50 -k mapbase_${VERSION}.publickey.vdf -K mapbase_${VERSION}.privatekey.vdf -vpk \"${VPK_CONTENT_DIR}/hl2_mapbase_content\""
-eval "${BIN_DIR}/vpk.exe rehash \"${VPK_CONTENT_DIR}/hl2_mapbase_content\""
-echo "Moving hl2_mapbase_content"
-mv VPKs/*.vpk ${BUILD_DIR}/mapbase_hl2/content
+eval "${BIN_DIR}/vpk.exe -M -P -c 50 -k mapbase2_${VERSION}.publickey.vdf -K mapbase2_${VERSION}.privatekey.vdf -vpk \"${VPK_CONTENT_DIR}/mb2_shared_shaders_${VERSION_VPK_NAME}\""
+eval "${BIN_DIR}/vpk.exe rehash \"${VPK_CONTENT_DIR}/mb2_shared_shaders_${VERSION_VPK_NAME}\""
+echo "Moving mb2_shared_shaders_${VERSION_VPK_NAME}"
+mv VPKs/*.vpk ${BUILD_DIR}/mapbase2_shared
 
-eval "${BIN_DIR}/vpk.exe -M -P -c 50 -k mapbase_${VERSION}.publickey.vdf -K mapbase_${VERSION}.privatekey.vdf -vpk \"${VPK_CONTENT_DIR}/hl2_materials\""
-eval "${BIN_DIR}/vpk.exe rehash \"${VPK_CONTENT_DIR}/hl2_materials\""
-echo "Moving hl2_materials"
-mv VPKs/*.vpk ${BUILD_DIR}/mapbase_hl2/content
+eval "${BIN_DIR}/vpk.exe -M -P -c 50 -k mapbase2_${VERSION}.publickey.vdf -K mapbase2_${VERSION}.privatekey.vdf -vpk \"${VPK_CONTENT_DIR}/mb2_hl2_content\""
+eval "${BIN_DIR}/vpk.exe rehash \"${VPK_CONTENT_DIR}/mb2_hl2_content\""
+echo "Moving mb2_hl2_content"
+mv VPKs/*.vpk ${BUILD_DIR}/mapbase2_hl2/content
 
-eval "${BIN_DIR}/vpk.exe -M -P -c 50 -k mapbase_${VERSION}.publickey.vdf -K mapbase_${VERSION}.privatekey.vdf -vpk \"${VPK_CONTENT_DIR}/lostcoast_materials\""
-eval "${BIN_DIR}/vpk.exe rehash \"${VPK_CONTENT_DIR}/lostcoast_materials\""
-echo "Moving lostcoast_materials"
-mv VPKs/*.vpk ${BUILD_DIR}/mapbase_hl2/content
+eval "${BIN_DIR}/vpk.exe -M -P -c 50 -k mapbase2_${VERSION}.publickey.vdf -K mapbase2_${VERSION}.privatekey.vdf -vpk \"${VPK_CONTENT_DIR}/mb2_hl2mp_content\""
+eval "${BIN_DIR}/vpk.exe rehash \"${VPK_CONTENT_DIR}/mb2_hl2mp_content\""
+echo "Moving mb2_hl2mp_content"
+mv VPKs/*.vpk ${BUILD_DIR}/mapbase2_hl2mp/content
 
-eval "${BIN_DIR}/vpk.exe -M -P -c 50 -k mapbase_${VERSION}.publickey.vdf -K mapbase_${VERSION}.privatekey.vdf -vpk \"${VPK_CONTENT_DIR}/episodic_mapbase_content\""
-eval "${BIN_DIR}/vpk.exe rehash \"${VPK_CONTENT_DIR}/episodic_mapbase_content\""
-echo "Moving episodic_mapbase_content"
-mv VPKs/*.vpk ${BUILD_DIR}/mapbase_episodic/content
+eval "${BIN_DIR}/vpk.exe -M -P -c 50 -k mapbase2_${VERSION}.publickey.vdf -K mapbase2_${VERSION}.privatekey.vdf -vpk \"${VPK_CONTENT_DIR}/mb2_episodic_content\""
+eval "${BIN_DIR}/vpk.exe rehash \"${VPK_CONTENT_DIR}/mb2_episodic_content\""
+echo "Moving mb2_episodic_content"
+mv VPKs/*.vpk ${BUILD_DIR}/mapbase2_episodic/content
 
-eval "${BIN_DIR}/vpk.exe -M -P -c 50 -k mapbase_${VERSION}.publickey.vdf -K mapbase_${VERSION}.privatekey.vdf -vpk \"${VPK_CONTENT_DIR}/episodic_materials\""
-eval "${BIN_DIR}/vpk.exe rehash \"${VPK_CONTENT_DIR}/episodic_materials\""
-echo "Moving episodic_materials"
-mv VPKs/*.vpk ${BUILD_DIR}/mapbase_episodic/content
+eval "${BIN_DIR}/vpk.exe -M -P -c 50 -k mapbase2_${VERSION}.publickey.vdf -K mapbase2_${VERSION}.privatekey.vdf -vpk \"${VPK_CONTENT_DIR}/mb2_tf_content\""
+eval "${BIN_DIR}/vpk.exe rehash \"${VPK_CONTENT_DIR}/mb2_tf_content\""
+echo "Moving mb2_tf_content"
+mv VPKs/*.vpk ${BUILD_DIR}/mapbase2_tf/content
 
-if [ "$USE_LOCAL_CODE" = true ] || [ -d VPKs/hl2_scenes ] ;
+if [ "$USE_LOCAL_CODE" = true ] || [ -d VPKs/mb2_hl2_scenes ] ;
 then
-	eval "${BIN_DIR}/vpk.exe -M -P -c 50 -k mapbase_${VERSION}.publickey.vdf -K mapbase_${VERSION}.privatekey.vdf -vpk \"${VPK_CONTENT_DIR}/hl2_scenes\""
-	eval "${BIN_DIR}/vpk.exe rehash \"${VPK_CONTENT_DIR}/hl2_scenes\""
-	echo "Moving hl2_scenes"
-	mv VPKs/*.vpk ${BUILD_DIR}/mapbase_hl2/content
+	eval "${BIN_DIR}/vpk.exe -M -P -c 50 -k mapbase2_${VERSION}.publickey.vdf -K mapbase2_${VERSION}.privatekey.vdf -vpk \"${VPK_CONTENT_DIR}/mb2_hl2_scenes\""
+	eval "${BIN_DIR}/vpk.exe rehash \"${VPK_CONTENT_DIR}/mb2_hl2_scenes\""
+	echo "Moving mb2_hl2_scenes"
+	mv VPKs/*.vpk ${BUILD_DIR}/mapbase2_hl2/content
 	
-	eval "${BIN_DIR}/vpk.exe -M -P -c 50 -k mapbase_${VERSION}.publickey.vdf -K mapbase_${VERSION}.privatekey.vdf -vpk \"${VPK_CONTENT_DIR}/episodic_scenes\""
-	eval "${BIN_DIR}/vpk.exe rehash \"${VPK_CONTENT_DIR}/episodic_scenes\""
-	echo "Moving episodic_scenes"
-	mv VPKs/*.vpk ${BUILD_DIR}/mapbase_episodic/content
+	eval "${BIN_DIR}/vpk.exe -M -P -c 50 -k mapbase2_${VERSION}.publickey.vdf -K mapbase2_${VERSION}.privatekey.vdf -vpk \"${VPK_CONTENT_DIR}/mb2_episodic_scenes\""
+	eval "${BIN_DIR}/vpk.exe rehash \"${VPK_CONTENT_DIR}/mb2_episodic_scenes\""
+	echo "Moving mb2_episodic_scenes"
+	mv VPKs/*.vpk ${BUILD_DIR}/mapbase2_episodic/content
 fi
 
 if [ "$USE_LOCAL_FGDS" = true ] ;
@@ -236,14 +249,18 @@ then
 	echo "Copying FGDs"
 	echo "====================="
 
-	mkdir ${BUILD_DIR}/mapbase_shared/shared_misc
-	mkdir ${BUILD_DIR}/mapbase_shared/shared_misc/bin
+	mkdir ${BUILD_DIR}/mapbase2_shared/shared_misc
+	mkdir ${BUILD_DIR}/mapbase2_shared/shared_misc/bin
 
-	cp ${SOURCE_FGDS_DIR}/base.fgd ${BUILD_DIR}/mapbase_shared/shared_misc/bin
+	cp ${SOURCE_FGDS_DIR}/base.fgd ${BUILD_DIR}/mapbase2_shared/shared_misc/bin
 	echo "base.fgd copied"
-	cp ${SOURCE_FGDS_DIR}/halflife2.fgd ${BUILD_DIR}/mapbase_shared/shared_misc/bin
+	cp ${SOURCE_FGDS_DIR}/halflife2.fgd ${BUILD_DIR}/mapbase2_shared/shared_misc/bin
 	echo "halflife2.fgd copied"
-	cp ${SOURCE_FGDS_DIR}/hammerplusplus_fgd_mapbase.fgd ${BUILD_DIR}/mapbase_shared/shared_misc/bin
+	cp ${SOURCE_FGDS_DIR}/hl2mp.fgd ${BUILD_DIR}/mapbase2_shared/shared_misc/bin
+	echo "hl2mp.fgd copied"
+	cp ${SOURCE_FGDS_DIR}/tf.fgd ${BUILD_DIR}/mapbase2_shared/shared_misc/bin
+	echo "tf.fgd copied"
+	cp ${SOURCE_FGDS_DIR}/hammerplusplus_fgd_mapbase.fgd ${BUILD_DIR}/mapbase2_shared/shared_misc/bin
 	echo "hammerplusplus_fgd_mapbase.fgd copied"
 fi
 
@@ -260,26 +277,19 @@ echo "====================="
 echo "Cleaning up files"
 echo "====================="
 
-rm -r VPKs/episodic_materials
-echo "Loose episodic_materials removed"
-rm -r VPKs/hl2_materials
-echo "Loose hl2_materials removed"
-rm -r VPKs/lostcoast_materials
-echo "Loose lostcoast_materials removed"
-
 if [ "$USE_LOCAL_CODE" = true ] ;
 then
-	rm -r VPKs/episodic_scenes
-	echo "Loose episodic_scenes removed"
-	rm -r VPKs/hl2_scenes
-	echo "Loose hl2_scenes removed"
+	rm -r VPKs/mb2_episodic_scenes
+	echo "Loose mb2_episodic_scenes removed"
+	rm -r VPKs/mb2_hl2_scenes
+	echo "Loose mb2_hl2_scenes removed"
 fi
 
-mkdir ${BUILD_DIR}/mapbase_episodic/custom
-mkdir ${BUILD_DIR}/mapbase_hl2/custom
+mkdir ${BUILD_DIR}/mapbase2_episodic/custom
+mkdir ${BUILD_DIR}/mapbase2_hl2/custom
 
-mkdir ${BUILD_DIR}/mapbase_episodic/maps
-mkdir ${BUILD_DIR}/mapbase_hl2/maps
+mkdir ${BUILD_DIR}/mapbase2_episodic/maps
+mkdir ${BUILD_DIR}/mapbase2_hl2/maps
 
 echo "=========================================="
 echo "Manual steps needed:"
@@ -288,33 +298,28 @@ echo "=========================================="
 if [ "$USE_LOCAL_CODE" = false ] ;
 then
 	echo "	- ADD THE CODE"
-	echo "		- Add binaries to mapbase_hl2 and mapbase_episodic"
+	echo "		- Add binaries to mapbase2_hl2 and mapbase2_episodic"
 	echo "		- If applicable, add compile tools"
 	
-	if [ ! -e ${BUILD_DIR}/mapbase_hl2/content/hl2_scenes_dir.vpk ] && [ ! -e ${BUILD_DIR}/mapbase_episodic/content/episodic_scenes_dir.vpk ]
+	if [ ! -e ${BUILD_DIR}/mapbase2_hl2/content/mb2_hl2_scenes_dir.vpk ] && [ ! -e ${BUILD_DIR}/mapbase2_episodic/content/mb2_hl2_scenes_dir.vpk ]
 	then
-		echo "	- Add hl2_scenes and/or episodic_scenes VPKs to the respective games' content directories"
+		echo "	- Add mb2_hl2_scenes and/or mb2_hl2_scenes_dir VPKs to the respective games' content directories"
 	fi
 else
-	if [ ! -e ${BUILD_DIR}/mapbase_episodic/bin/game_shader_dx9.dll ] || [ ! -e ${BUILD_DIR}/mapbase_hl2/bin/game_shader_dx9.dll ]
+	if [ ! -e ${BUILD_DIR}/mapbase2_episodic/bin/x64/game_shader_dx9.dll ] || [ ! -e ${BUILD_DIR}/mapbase2_hl2/bin/x64/game_shader_dx9.dll ]
 	then
 		echo "	- Add missing shader DLLs"
 	fi
 
-	if [ ! -e ${BUILD_DIR}/mapbase_hl2/bin/server.so ]
+	if [ ! -e ${BUILD_DIR}/mapbase2_hl2/bin/linux64/server.so ]
 	then
 		echo "	- Add missing Linux SOs (if this build should support Linux)"
 	fi
 fi
 
-if [ ! -e ${BUILD_DIR}/mapbase_shared/shared_misc/bin/base.fgd ]
+if [ ! -e ${BUILD_DIR}/mapbase2_shared/shared_misc/bin/base.fgd ]
 then
-	echo "	- Add the FGDs to mapbase_shared/shared_misc/bin"
-fi
-
-if [ ! -e ${BUILD_DIR}/mapbase_shared/shared_content_dir.vpk ]
-then
-	echo "	- Add legacy pre-v7.0 shared_content_dir.vpk to mapbase_shared (find from existing version of build)"
+	echo "	- Add the FGDs to mapbase2_shared/shared_misc/bin"
 fi
 
 echo "=========================================="
